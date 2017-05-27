60c8b407ac92a6079458958c38bce621ace944bc
kgomez0110 <kennethgomez2020@u.northwestern.edu>
2017-05-27 15:16:30 -0500

    Update newParserTest.py
    This parser is a lot more condensed and just better in every way lol.

--- newParserTest.py
+++ newParserTest.py
@@ -2,9 +2,9 @@
 message = input("input instruction line: ")
 instructionDict = {}
 instructionArray = message.split(",")
-codeArray = []
+codeArray = [0] * len(instructionArray)
 size  = 0
-index = 0
+ii = 0
 for keys in instructionArray :
     sensor_key = keys.split(":")
     instructionDict[sensor_key[0]] = sensor_key[1]
@@ -12,5 +12,7 @@ for keys in instructionArray :
     size = size + 1
 while ii < size :
     print("Code: ", codeArray[ii], " Value: ", instructionDict[codeArray[ii]])
+    ii = ii + 1
 
     
+
