d0810ef6e8582e40d746308c2bf2c9d2d83a4446
kgomez0110 <kennethgomez2020@u.northwestern.edu>
2017-06-03 21:27:04 +0000

    Update Parser.py
    Added the correct formatting for the return code.
    Used byte arrays so it actually works with serial
    Some janky checks in the beginning so it only reads the instruction codes

--- Parser.py
+++ Parser.py
@@ -1,56 +1,69 @@
+#!/usr/bin/python3
+
 import serial
 
-ser.serial = serial.Serial('/dev/ttyACM0', 9600)
-message = ser.readline()
-sensorDict = {}
-actuatorDict = {}
-instructionArray = message.split(",")
-codeArray = [0] * len(instructionArray)
-size  = 0
-ii = 0
-for keys in instructionArray :
-    sensor_key = keys.split(":")
-    if(sensorKey[0].index(1) == "S") :
-        sensorDict[sensor_key[0]] = sensor_key[1]
-    elif(sensorKey[1].index(1) == "A")
-        actuatorDict[sensor_key[0]] = sensor_key[1]
-    codeArray[size] = sensor_key[0]
-    size += 1
-while ii < size :
-    print("Code: ", codeArray[ii], " Value: ", instructionDict[codeArray[ii]])
-    ii += 1
+ser = serial.Serial('/dev/ttyACM0', 9600)
+while True :
+    message = str(ser.readline())
+    if(len(message) > 6) :
+        if(message[6] == "Y"):
+           # message = input("input instruction: ")
+            sensorDict = {}
+            actuatorDict = {}
+            instructionArray = message.split(",")
+            codeArray = [0] * len(instructionArray)
+            size  = 0
+            sensorSize = 0
+            actuatorSize = 0
+            ii = 1
+            for keys in instructionArray :
+                sensor_key = keys.split(":")
+                if(sensor_key[0][1] == "S") :
+                    sensorDict[sensor_key[0]] = sensor_key[1]
+                    sensorSize += 1
+                elif(sensor_key[0][1]== "A") :
+                    actuatorDict[sensor_key[0]] = sensor_key[1]
+                    actuatorSize += 1
+                codeArray[size] = sensor_key[0]
+                size += 1
+            while ii <= sensorSize :
+                print("Code: ", codeArray[ii], " Value: ", sensorDict[codeArray[ii]])
+                ii += 1
+            while ii < actuatorSize + sensorSize:
+                print("Code: ", codeArray[ii], " Value: ", actuatorDict[codeArray[ii]])
+                ii += 1
 
-maxDict = {}
-minDict = {}
-sensor_actuatorDict = {}
-def readTextFile(fileName) :
-    file = open(fileName)
-    for sensor in file :
-        sensor = sensor.strip()
-        values = sensor.split(",")
-        minDict[values[0]] = float(values[1])
-        maxDict[values[0]] = float(values[2])
-        sensor_actuatorDict[values[0]] = values[3]
+            maxDict = {}
+            minDict = {}
+            sensor_actuatorDict = {}
+            def readTextFile(fileName) :
+                file = open(fileName)
+                for sensor in file :
+                    sensor = sensor.strip()
+                    values = sensor.split(",")
+                    minDict[values[0]] = float(values[1])
+                    maxDict[values[0]] = float(values[2])
+                    sensor_actuatorDict[values[0]] = values[3]
 
-readTextFile("recipe.txt")
-for sensor in codeArray :
-    if(float(sensorDict[sensor]) > maxDict[sensor]) :
-        actuatorDict[sensor_actuatorDict[sensor]] = "0"; # Turn off
-    elif(float(instructionDict[sensor]) < minDict[sensor]) :
-        actuatorDict[sensor_actuatorDict[sensor]] = "1"; # Turn on
+            readTextFile("testRecipe.txt")
+            for sensor in codeArray :
+                if(sensor[1] == "S") :
+                    if(float(sensorDict[sensor]) > maxDict[sensor]) :
+                        actuatorDict[sensor_actuatorDict[sensor]] = "0"; # Turn off
+                    elif(float(sensorDict[sensor]) < minDict[sensor]) :
+                        actuatorDict[sensor_actuatorDict[sensor]] = "1"; # Turn on
 
-returnMessage = "GTYP\":\"Stream\","
-for code in codeArray :
-    if(code.index(1) == "S") :
-        returnMessage += code
-        returnMessage += ":"
-        returnMessage += sensorDict[code]
-        returnMessage += ","
-    elif(code.index(1) == "A") :
-        returnMessage += code
-        returnMessage += ":"
-        returnMessage += actuatorDict[code]
-        returnMessage += ","
-returnMessage += "\"GEND\":0"
-ser.write(returnMessage)
+
+
+            returnMessage = ""
+            for code in codeArray :
+                if(code[1] == "A") :
+                    returnMessage += code[1:7]
+                    returnMessage += " "
+                    returnMessage += actuatorDict[code]
+                    print(returnMessage)
+                    x = bytearray(returnMessage+"\n","UTF-8")
+                    ser.write(x)
+                    returnMessage = ""
+
 
